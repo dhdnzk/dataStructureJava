303a4e53abb3389588f31976f6a31b704538abef
DongHoon <dhdnzk0304@gmail.com>
2015-10-08 20:43:09 +0900

    LinkedList 클래스 상속, 생성자 코딩, 나머지 메소드 미완

--- linkedlist/CirlceLinkedList.java
+++ linkedlist/CirlceLinkedList.java
@@ -5,4 +5,31 @@ package datastructure.linkedlist;
  */
 public class CirlceLinkedList extends LinkedList {
 
+    CirlceLinkedList () {
+        super( );
+
+    }
+
+    CirlceLinkedList ( int data ) {
+        super( data );
+        this.head.next.next = this.head.next;
+    }
+
+    void NodeAddOnTail ( int data ) {
+        this.tail.next = new Node();
+        this.tail = this.tail.next;
+        this.tail.data = data;
+        this.tail.next = this.head.next;
+        this.cur = null;
+        this.before = null;
+        this.numOfData ++;
+    }
+
+    void NodeAddOnHead ( int data ) {
+        Node newNode = new Node();
+        newNode.data = data;
+        this.tail.next = newNode;
+    }
+
+
 }
